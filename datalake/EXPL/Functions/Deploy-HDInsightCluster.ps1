fe832ab0b1f2fab566e1eabaa8ac2915078e92a0
KHALIFA Yoann <y.khalifa@aaa-data.fr>
2019-03-15 14:45:07 +0100

    clean files

--- datalake/EXPL/Functions/Deploy-HDInsightCluster.ps1
+++
@@ -1,7 +0,0 @@
-# Start the deployment
-Write-Host "Starting deployment...";
-if(Test-Path $parametersFilePath) {
-    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -TemplateParameterFile $parametersFilePath;
-} else {
-    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath;
-}
